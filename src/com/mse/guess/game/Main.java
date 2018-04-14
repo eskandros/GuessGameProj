e8d1495a6cb1676b8ab8c54e3eead6ce6cb3a610
eskandros <meskandros@bigpond.com>
2018-04-14 23:56:52 +1000

    Update the Main class

--- src/com/mse/guess/game/Main.java
+++ src/com/mse/guess/game/Main.java
@@ -3,6 +3,7 @@ package com.mse.guess.game;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    GuessGame gg = new GuessGame();
+        gg.startGame();
     }
 }
