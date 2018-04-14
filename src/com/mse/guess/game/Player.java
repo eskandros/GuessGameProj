f7e7f0c368febe5fcc8b7716bdfd1d17b430657a
eskandros <meskandros@bigpond.com>
2018-04-14 23:40:20 +1000

    Update Player class

--- src/com/mse/guess/game/Player.java
+++ src/com/mse/guess/game/Player.java
@@ -4,10 +4,9 @@ package com.mse.guess.game;
  * Created by mseskander on 14/04/2018.
  */
 public class Player {
-    private int number;
 
     public int guess() {
-        number = (int) (Math.random() * 10);
+        int number = (int) (Math.random() * 10);
         System.out.println("I am guessing " + number);
 
         return number;
