0663afad95e79df383f96d65ae697c329a5c7be3
eskandros <meskandros@bigpond.com>
2018-04-15 21:24:21 +1000

    Update Main class.

--- src/com/mse/Main.java
+++ src/com/mse/Main.java
@@ -1,5 +1,6 @@
 package com.mse;
 
+import com.mse.arrays.book;
 import com.mse.guess.game.GuessGame;
 
 public class Main {
@@ -10,8 +11,32 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // user these lines to run the guess game
-	    GuessGame game = new GuessGame();
-        game.startGame();
+        // use these lines to run the guess game
+	     GuessGame game = new GuessGame();
+         game.startGame();
+
+        // use these lines to run the book array
+//        book[] myBooks = new book[3];
+//
+//        myBooks[0] = new book();
+//        myBooks[0].setTitle("Java");
+//        myBooks[1] = new book();
+//        myBooks[1].setTitle("The Java Cookbook");
+//        myBooks[2] = new book();
+//        myBooks[2].setTitle("The Java Webservice");
+//
+//        myBooks[0].setAuthor("Terry");
+//        myBooks[1].setAuthor("Bob");
+//        myBooks[2].setAuthor("Andy");
+//
+//        int i = 0;
+//        while (i < myBooks.length) {
+//
+//            System.out.print(myBooks[i].getTitle());
+//            System.out.print( " by " );
+//            System.out.println(myBooks[i].getAuthor());
+//            i++;
+//        }
+
     }
 }
