15e1341d7c8bda62f6a9010dfce7ecb31f89bba8
eskandros <meskandros@bigpond.com>
2018-04-15 16:11:06 +1000

    Add book class.

---
+++ src/com/mse/arrays/book.java
@@ -0,0 +1,25 @@
+package com.mse.arrays;
+
+/**
+ * Created by mseskander on 15/04/2018.
+ */
+public class book {
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
