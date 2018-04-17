2a37e2dd52e67df7b478c5dfb998e6de2bb95ba9
eskandros <meskandros@bigpond.com>
2018-04-17 10:20:27 +1000

    Update Main class.

--- src/com/mse/Main.java
+++ src/com/mse/Main.java
@@ -12,31 +12,43 @@ public class Main {
      */
     public static void main(String[] args) {
         // use these lines to run the guess game
-	     GuessGame game = new GuessGame();
-         game.startGame();
+        playGuessGame();
+
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
 
         // use these lines to run the book array
-//        book[] myBooks = new book[3];
-//
-//        myBooks[0] = new book();
-//        myBooks[0].setTitle("Java");
-//        myBooks[1] = new book();
-//        myBooks[1].setTitle("The Java Cookbook");
-//        myBooks[2] = new book();
-//        myBooks[2].setTitle("The Java Webservice");
-//
-//        myBooks[0].setAuthor("Terry");
-//        myBooks[1].setAuthor("Bob");
-//        myBooks[2].setAuthor("Andy");
-//
-//        int i = 0;
-//        while (i < myBooks.length) {
-//
-//            System.out.print(myBooks[i].getTitle());
-//            System.out.print( " by " );
-//            System.out.println(myBooks[i].getAuthor());
-//            i++;
-//        }
+        arrayPractice();
 
+    }
+
+    private static void playGuessGame() {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+
+    private static void arrayPractice() {
+
+        book[] myBooks = new book[3];
+
+        myBooks[0] = new book();
+        myBooks[0].setTitle("Java");
+        myBooks[1] = new book();
+        myBooks[1].setTitle("The Java Cookbook");
+        myBooks[2] = new book();
+        myBooks[2].setTitle("The Java Webservice");
+
+        myBooks[0].setAuthor("Terry");
+        myBooks[1].setAuthor("Bob");
+        myBooks[2].setAuthor("Andy");
+
+        int i = 0;
+        while (i < myBooks.length) {
+
+            System.out.print(myBooks[i].getTitle());
+            System.out.print( " by " );
+            System.out.println(myBooks[i].getAuthor());
+            i++;
+        }
     }
 }
